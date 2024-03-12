31ded82b187951d0f42d4ad89b03be28a586f974
Md Bappy <mdbappy21.cse@gmail.com>
2024-03-13 00:06:47 +0600

    Assignment 01

---
+++ Assingment_01/Code/Song.dart
@@ -0,0 +1,10 @@
+import 'Media.dart';
+
+class Song extends Media {
+  String artist;
+  Song({required this.artist});
+  @override
+  void play() {
+    print("Playing song by $artist...");
+  }
+}
