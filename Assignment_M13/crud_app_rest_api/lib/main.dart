221c7240fe99abeab0bd4fd8750a5e8a9b4b4178
Md Bappy <mdbappy1350@gmail.com>
2024-05-25 19:08:30 +0600

    Assignment

---
+++ Assignment_M13/crud_app_rest_api/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:crud_app/screens/ProductListScreen.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(const CrudApp());
+}
+
+class CrudApp extends StatelessWidget {
+  const CrudApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const ProductListScreen(),
+      theme: ThemeData(
+          inputDecorationTheme: const InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.purple)
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.purple)
+            ),
+            errorBorder: OutlineInputBorder(),
+            focusedErrorBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.red)
+            ),
+          ),
+          elevatedButtonTheme:  ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size.fromWidth(double.maxFinite),
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)
+                ),
+                backgroundColor: Colors.pinkAccent,
+                foregroundColor: Colors.white,
+              )
+          )
+      ),
+    );
+  }
+}
